7ccff174aa31ced36d459e5681d2175889f1f837
Teacher Math <teacher@math.com>
2026-08-16 16:50:08 +0700

    Fix typecheck errors and update UI calendar component

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -54,12 +54,10 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
-          <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
-          <ChevronRight className={cn("h-4 w-4", className)} {...props} />
-        ),
+        Chevron: ({ className, orientation, ...props }: any) => {
+          const Comp = orientation === "left" ? ChevronLeft : ChevronRight;
+          return <Comp className={cn("h-4 w-4", className)} {...props} />;
+        },
       }}
       {...props}
     />
